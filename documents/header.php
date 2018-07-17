011b7941cb1ece804ae9fc3d72b411d56b8f93a1
LacySimpson <lacy.simpson@usm.edu>
2018-07-17 12:08:36 -0500

    php file that contains the header navigation menu for the website.

---
+++ documents/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<div class="home-page">
+     
+<head>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" href="stylesheet.css" />
+    <title>Lacy Simpson's Resume</title>
+    <script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script>
+</head>
+<body>
+    <div class="wrapper">   
+	<div id="header">
+		<ul>
+ 			<li><a href="index.php">Home</a></li>
+	        <li><a href="aboutme.php">About</a></li>
+  			<li><a href="portfolio.php">Portfolio</a></li>
+ 			<li><a href="contact.php">Contact</a></li>	 
+		</ul>		
+	</div>
