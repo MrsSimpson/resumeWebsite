0f67eddba24077b70bb8b21ce408eb8368df927f
LacySimpson <lacy.simpson@usm.edu>
2018-07-17 12:06:59 -0500

    php used to load the header from a seperate php file

---
+++ documents/portfolio.php
@@ -0,0 +1,73 @@
+<?php include "header.php" ?>
+
+	<div class="resume">
+        <h3><a href="Lacy_Simpson_06.20.2018 updated Resume copy.pdf"> My PDF Resume </a></h3>
+    </div>
+    <div class="portfolio">
+        <div class="experience">
+            <h1>Experience</h1>
+            <h2>Internship</h2>
+            <h3>Systems Engineering And Software Development | Huntington Ingalls Industries | May-August 2017</h3>
+            <h4>IT TEAM TEST LEAD ON MOBILITY PILOT</h4>
+            <p>
+                · Lead test support including end user testing, support, and issue resolution <br>
+                · Subject matter expert on various scanners, kiosks, and iOS devices<br>
+                · Developed Test Scripts executed by the end user to validate the new applications<br>
+                · QA Collaborator and product tester with DXC subcontractor and Ingalls code development test teams<br>
+                · Awarded “IT Employee of the Month”
+            </p>
+            <h4>ADDITIONAL SYSTEMS ENGINEERNG EXPERIENCE</h4>
+            <p>
+
+                · Assisted in Apprentice Evaluation Process software solution selection and presentation for the customer<br>
+                · Lead researcher for IT “Knowledge Now” topics using Gartner, world's leading information technology
+                research and advisory company<br>
+                · Shadowed functional and IT workstream leads in scoping phase of the SAP implementation; this
+                business management software initiative<br> is geared towards migration off of the mainframe
+            </p>
+            <h4>SOFTWARE DEVELOPMENT ROTATION</h4>
+            <p>
+                 · C# development building simple applications and learning various C# tools<br>
+                 · Hands on Experience performing Microsoft SQL Server queries in the test environment
+            </p>
+            
+        </div>
+        
+        <div class="education">
+            <h1>Education</h1>
+            <h3>B.S COMPUTER SCIENCE | UNIVERSITY OF SOUTHERN MISSISSIPPI | LONG BEACH, MS | May 2019</h3>
+            <p> 
+                · GPA 4.0 | President’s List (4 semesters) | Member of the Golden Key International Honor Society<br>
+                · Received: Academic Transfer Scholarship, Bruner Science & Tech Scholarship
+            </p>
+            <h3>A.A. | MISSISSIPPI GULF COAST COMMUNITY COLLEGE | GAUTIER, MS | MAY 2013</h3>
+            <p>
+                · GPA 3.5 | Dean’s list | Graduated with honors
+            </p>
+            
+        </div>
+        
+        <div class="projects">
+            <h1> Special Projects </h1>
+            <h3>iOS Mobile Application Development | Eagle Social</h3>
+            <p> 
+                ~ A brief background to the development of Eagle Social ~ <br>
+                During the Spring Semester of 2017 while taking Object Oriented Programming, I had the privilidge of working with a group of other like minded students that had the desire learn and experiment with iOS application development. We all wanted to learn Swift and create an application for our class project. We chose to create a social media application, Eagle Social, for students and faculity that could be used by the University. <br> Although the project was never presented to the school, we did complete the assignment, and we ended up with a fully functional iOS socail media application written in Swift using Google's Firebase as our non relational database. <br>
+                The project was a lot of fun to work on and also very challenging, but I learned quite a bit over the course of 5 months. I learned how to of take an idea and create something tangable and real. I also gained valuable experince working as a team on my first group development project. This project brought new challenges, such as learning GitHub, learning a new language, learning how to work with a non relational data base, and completing deliverables as if our project were in a real workforce development project. 
+            </p>
+        </div>
+        
+        <div class="community">
+            <h1> Community Service </h1>
+            <h3>Back Bay Missions | Community Event Volunteer | Biloxi, MS</h3>
+            <h3>Salvation Army | Painter | Pascagoula, MS</h3>
+            <h3>Youth Leader | Washington Missionary Baptist Chruch | Neely, MS</h3>
+            <p> 
+            </p>
+        </div>
+    </div>     
+    
+    </div>
+</body>
+
+</html>
