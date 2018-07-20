81bc10dd8b75bbf914271cc610bc5972e7250f18
LacySimpson <lacy.simpson@usm.edu>
2018-07-20 00:40:45 -0500

    added jQuery functionality that changes the about me image when the user hovers over and leaves the image. The jQuery code is located in a seperate file and called in the footer.php

--- documents/aboutme.php
+++ documents/aboutme.php
@@ -1,22 +1,14 @@
-<?php include "header.php" ?>
-
+<?php include "../documents/header.php" ?>
 	<div class="about">
-		<h2>About</h2>
-		<p> Hi! I'm Lacy, a student at the University of Southern Mississippi, and I am working on my degree in 
+		<h1>About</h1> <img id="selfie" src= "../images/selfie.jpg">
+		<p>  Hi! I'm Lacy, a student at the University of Southern Mississippi, and I am working on my degree in 
 			Computer Science with a minor in mathematics. I'm married to the love of my life, Cody, 
 			and we currently reside in D'Iberville, Mississippi.<br> 
 				When I'm not buried in books or studying for the next exam, I enjoy learning about new technologies,
 			reading fiction, playing video games with my husband, and spending time with our family.<br> 
 				I have a genuine passion for writing code, and I'm confident that I have found my calling. 
 			I enjoy working on development projects just for fun, but now that I am in my final year of school, I 
-			am preparing to put my skills to use and find a career that I am passionate about. 
+			am preparing to put my skills to use and find a career that I am passionate about.    		      		 
 		</p>
-        
 	</div>
-        <div class="profile_pic">
-          <img src= "selfie.jpg"> 
-        </div>
-        
-    </div>
-</body>
-</html>
+<?php include "../documents/footer.php" ?>
