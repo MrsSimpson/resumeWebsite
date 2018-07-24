c6a6d460edbf1d3a473427c56a06372f99a02a57
LacySimpson <lacy.simpson@usm.edu>
2018-07-23 23:01:57 -0500

    An html contact form was added to the page. This form has no functionality behind it, and is purely for the purpose of the assignment.

--- documents/contact.php
+++ documents/contact.php
@@ -1,10 +1,24 @@
 <?php include "../documents/header.php" ?>
 
 <div id="contact">
-	<h1><strong>Contact Me</strong></h1>
-    <h3>Check out my LinkedIn Profile:   <a href="https://www.linkedin.com/in/lacy-simpson/"><img src="../images/linkedin.png"></a></h3>
-	<h3>Email:<a href="mailto:lacy.simpson@usm.edu"> lacy.simpson@usm.edu</a></h3>
-	<h3>Call: xxx-xxx-xxxx</h3>
+	
+    <h2><strong>Check out my LinkedIn Profile: </strong>  
+	<a href="https://www.linkedin.com/in/lacy-simpson/"><img src="../images/linkedin.png"></a></h2>
+	<h2>Email Me:<a href="mailto:lacy.simpson@usm.edu"> lacy.simpson@usm.edu</a></h2>
+
+	<div id="contactform">
+		<form>
+		<h1><strong>Contact Me</strong></h1>
+			Name:<br>
+			<input type="text" name="name" class="inputField" required><br>
+			Email Address:<br>
+			<input type="email" name="email" class="inputField" required><br>
+			Message:<br>
+			<input type="textarea" name="message" class="textAreaInput" required><br>
+			<br>
+			<input type="submit" class="submitButton">
+		</form>
+	</div>
 
 </div>
 <?php include "../documents/footer.php" ?>
