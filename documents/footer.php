d5a27cf1f56f467eb1c93b83faa798db69821563
LacySimpson <lacy.simpson@usm.edu>
2018-07-23 23:02:55 -0500

    minor changes made to the footer for all pages

--- documents/footer.php
+++ documents/footer.php
@@ -1,8 +1,10 @@
-<div id="footer">     
-            <a href="https://www.linkedin.com/in/lacy-simpson/"><img src="linkedinThumb.png"></a>    
+<div id="footer">
+            <p>Lacy Simpson | The University of Southern Mississippi | CSS405 |    
+            <a href="https://github.com/MrsSimpson/resumeWebsite"><img src="../images/github_thumb.png"></a> 
+            </p>   
     </div>
     </div>
-
+    <script type="text/javascript" src="../documents/jquerycode.js"></script>
 </body>
 </div>
 </html>
