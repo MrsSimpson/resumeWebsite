455ae78e69599cda1008c5b8b82b6ef8d61fd674
LacySimpson <lacy.simpson@usm.edu>
2018-07-23 23:03:29 -0500

    minor changes made to the header for all pages

--- documents/header.php
+++ documents/header.php
@@ -4,17 +4,18 @@
      
 <head>
 	<meta charset="utf-8" />
-	<link rel="stylesheet" href="stylesheet.css" />
+	<link rel="stylesheet" href="../stylesheets/stylesheet.css" />
+	<script type="text/javascript" src="http://code.jquery.com/jquery-3.3.1.js"></script>
+	
     <title>Lacy Simpson's Resume</title>
-    <script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script>
 </head>
 <body>
     <div class="wrapper">   
 	<div id="header">
 		<ul>
- 			<li><a href="index.php">Home</a></li>
-	        <li><a href="aboutme.php">About</a></li>
-  			<li><a href="portfolio.php">Portfolio</a></li>
- 			<li><a href="contact.php">Contact</a></li>	 
+ 			<li><a href="../documents/index.php">Home</a></li>
+	        <li><a href="../documents/aboutme.php">About</a></li>
+  			<li><a href="../documents/portfolio.php">Portfolio</a></li>
+ 			<li><a href="../documents/contact.php">Contact</a></li>	 
 		</ul>		
 	</div>
