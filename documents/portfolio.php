d80ab183f706070ed436fc1dea57407aeaed9852
LacySimpson <lacy.simpson@usm.edu>
2018-07-23 23:05:27 -0500

    Skill section added to the portfolio page which includes my skillset/languages. I also decided not to use the cursive font for this page. It seemed to make it harder to read. The page seems to flow much more smoothly.

--- documents/portfolio.php
+++ documents/portfolio.php
@@ -4,6 +4,26 @@
         <h3><a href="../documents/lacy_simpson_resume.pdf"> My PDF Resume </a></h3>
     </div>
     <div class="portfolio">
+        <div class="skills">
+            <h1>Skills And Programming Languages</h1>
+            <p>
+                Server Side: 
+                C++     |
+                Java    |
+                C#      |
+                Visual Basic    |
+                Swift       |
+                PHP<br>
+                Client Side:
+                HTML5  |
+                CSS3 |
+                JavaScript<br>
+                Database: 
+                MySQL   |
+                Microsoft SQL Server |
+                Firebase            
+            </p>
+        </div>
         <div class="experience">
             <h1>Experience</h1>
             <h2>Internship</h2>
@@ -52,8 +72,17 @@
             <h3>iOS Mobile Application Development | Eagle Social</h3>
             <p> 
                 ~ A brief background to the development of Eagle Social ~ <br>
-                During the Spring Semester of 2017 while taking Object Oriented Programming, I had the privilidge of working with a group of other like minded students that had the desire learn and experiment with iOS application development. We all wanted to learn Swift and create an application for our class project. We chose to create a social media application, Eagle Social, for students and faculity that could be used by the University. <br> Although the project was never presented to the school, we did complete the assignment, and we ended up with a fully functional iOS socail media application written in Swift using Google's Firebase as our non relational database. <br>
-                The project was a lot of fun to work on and also very challenging, but I learned quite a bit over the course of 5 months. I learned how to of take an idea and create something tangable and real. I also gained valuable experince working as a team on my first group development project. This project brought new challenges, such as learning GitHub, learning a new language, learning how to work with a non relational data base, and completing deliverables as if our project were in a real workforce development project. 
+                During the Spring Semester of 2017 while taking Object Oriented Programming, I had the privilege of working with 
+                a group of other like minded students that had the desire to learn and experiment with iOS application development. 
+                We all wanted to learn Swift and create an application for our class project. We chose to create a social media application,
+                 Eagle Social, for students and faculty that could be used by the University. <br> Although the project was never presented
+                  to the school, we did complete the assignment, and we ended up with a fully functional iOS social media application 
+                  written in Swift using Google's Firebase as our non relational database. <br>
+                The project was a lot of fun to work on and also very challenging, but I learned quite a bit over the course of 5 
+                months. I learned how to of take an idea and create something tangible and real. I also gained valuable experience 
+                working as a team on my first group development project. This project brought new challenges, such as learning GitHub, 
+                learning a new language, learning how to work with a non relational data base, and completing deliverables as if our project
+                 were in a real workforce development project. 
             </p>
         </div>
         
