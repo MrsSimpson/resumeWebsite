71f6e3734fe704fa0c6b2e3044c23b4d62f90eb8
LacySimpson <lacy.simpson@usm.edu>
2018-07-25 13:25:45 -0500

    script tags for jquery file added to the footer.

--- documents/footer.php
+++ documents/footer.php
@@ -1,6 +1,6 @@
 <div id="footer">
             <p>Lacy Simpson | The University of Southern Mississippi | CSS405 |    
-            <a href="https://github.com/MrsSimpson/resumeWebsite"><img src="../images/github_thumb.png"></a> 
+                <a href="https://github.com/MrsSimpson/resumeWebsite"><img src="../images/github_thumb.png"></a> 
             </p>   
     </div>
     </div>
