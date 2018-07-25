151ab6c3dee3b03488525f76bd9d210ea7018865
LacySimpson <lacy.simpson@usm.edu>
2018-07-25 13:26:57 -0500

    Padding was added to the header tags to keep the header elements from moving when the user scrolls on a larger screen.

--- documents/index.php
+++ documents/index.php
@@ -1,7 +1,9 @@
 <?php include "../documents/header.php" ?>
 		<div class="home">	
+
 			<h1>Lacy Simpson</h1>
 			<h4>Software Developer</h4>
+						
 		</div>
 <?php include "../documents/footer.php" ?>
 
